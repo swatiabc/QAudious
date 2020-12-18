eb0d9d74db4455fc5cc7b5b1992d483cbf369f6f
swatiabc <46042312+swatiabc@users.noreply.github.com>
2020-12-18 18:56:06 +0530

    changes

--- qaudious/settings.py
+++ qaudious/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'qaudious.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.m',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
