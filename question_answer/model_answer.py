b6696bf3d3c711a6cc1145e090a93384590cfa90
swatiabc <46042312+swatiabc@users.noreply.github.com>
2021-01-15 01:46:53 +0530

    question_answer done

---
+++ question_answer/model_answer.py
@@ -0,0 +1,2 @@
+def get_answer():
+    return "answer not found yet!!!!"
--- question_answer/views.py
+++ question_answer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
 
+from . import model_answer
 from . import models
 from transcribe_audio.models import AudioDataModel
 
@@ -15,15 +16,34 @@ def qa_display_page(request):
     return render(request,"qa_display.html")
 
 
+@require_http_methods(['GET', 'POST'])
 def qa_delete_confirm_page(request):
-    return render(request, "qa_delete_confirm.html")
+    if 'qa_data' not in request.session:
+        return HttpResponseRedirect("post_question.html")
+    
+    if request.method == 'GET': 
+        return render(request,"qa_delete_confirm.html")
+
+    try:
+        qa_data = models.QADataModel.objects.get(id=request.session['qa_data'])
+    except (KeyError, models.QADataModel.DoesNotExist):
+        qa_data = None    
+    
+    models.QADataModel.objects.filter(id=qa_data.id).delete()
+    del request.session["qa_data"]
+    
+    return render(request, "qa_deleted.html")
 
 
 def qa_deleted_page(request):
+    if 'qa_data' not in request.session:
+        return HttpResponseRedirect("post_question.html")
+
     return render(request,"qa_deleted.html")
 
 def qa_saved_page(request):
     return render(request,"qa_saved.html")
+
 
 @require_http_methods(['GET', 'POST'])
 def post_question(request):
@@ -36,8 +56,6 @@ def post_question(request):
     print("post question-------------------",request.method)
     if request.method == 'GET': 
         return render(request,"post_question.html")
-    
-    
 
     print("post question-------------------",request.method)
     uploaded_question = request.POST['uploaded_question']
@@ -51,5 +69,7 @@ def post_question(request):
     print("---------------------------------------------")
     print(qa_data.transcript.transcript)
     print("---------------------------------------------")
+    qa_data.answer = model_answer.get_answer()
     qa_data.save()
+    request.session["qa_data"]=qa_data.id
     return HttpResponseRedirect("qa_display.html")
