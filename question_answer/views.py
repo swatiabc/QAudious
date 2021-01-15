4960d8285a7fd1777b69e0466b89a6e58e40f5f9
swatiabc <46042312+swatiabc@users.noreply.github.com>
2021-01-15 13:18:22 +0530

    delete

--- question_answer/views.py
+++ question_answer/views.py
@@ -12,33 +12,38 @@ def question_page(request):
     return render(request,"question_2.html")
 
 
-def qa_display_page(request):
-    return render(request,"qa_display.html")
-
-
 @require_http_methods(['GET', 'POST'])
-def qa_delete_confirm_page(request):
-    if 'qa_data' not in request.session:
+def qa_display_page(request):
+    if 'question' not in request.session:
         return HttpResponseRedirect("post_question.html")
-    
-    if request.method == 'GET': 
-        return render(request,"qa_delete_confirm.html")
 
+    if request.method == 'GET': 
+        return render(request,"qa_display.html")
+    
+    qa_data = models.QADataModel()
+    qa_data.question = request.session["question"]
     try:
-        qa_data = models.QADataModel.objects.get(id=request.session['qa_data'])
-    except (KeyError, models.QADataModel.DoesNotExist):
-        qa_data = None    
+        qa_data.transcript = AudioDataModel.objects.get(id=request.session['transcript'])
+    except (KeyError, AudioDataModel.DoesNotExist):
+        qa_data.transcript = None
     
-    models.QADataModel.objects.filter(id=qa_data.id).delete()
-    del request.session["qa_data"]
-    
-    return render(request, "qa_deleted.html")
+    qa_data.answer = model_answer.get_answer()
+    qa_data.save()
+
+    return render(request,"qa_saved.html")
+
+
+
+def qa_delete_confirm_page(request):
+    if 'question' not in request.session:
+        return HttpResponseRedirect("post_question.html")
+    return render(request, "qa_delete_confirm.html")
 
 
 def qa_deleted_page(request):
-    if 'qa_data' not in request.session:
+    if 'question' not in request.session:
         return HttpResponseRedirect("post_question.html")
-
+    del request.session["question"]
     return render(request,"qa_deleted.html")
 
 def qa_saved_page(request):
@@ -51,25 +56,19 @@ def post_question(request):
     if 'transcript' not in request.session:
         return HttpResponseRedirect("upload.html")
 
-    abc = AudioDataModel.objects.get(id=request.session['transcript'])  
-    print("---------------------------------session",abc.id)  
-    print("post question-------------------",request.method)
     if request.method == 'GET': 
         return render(request,"post_question.html")
 
-    print("post question-------------------",request.method)
     uploaded_question = request.POST['uploaded_question']
-    qa_data = models.QADataModel()
-    qa_data.question = uploaded_question
-    try:
-        qa_data.transcript = AudioDataModel.objects.get(id=request.session['transcript'])
-    except (KeyError, AudioDataModel.DoesNotExist):
-        qa_data.transcript = None
+    # qa_data = models.QADataModel()
+    # qa_data.question = uploaded_question
+    # try:
+    #     qa_data.transcript = AudioDataModel.objects.get(id=request.session['transcript'])
+    # except (KeyError, AudioDataModel.DoesNotExist):
+    #     qa_data.transcript = None
     
-    print("---------------------------------------------")
-    print(qa_data.transcript.transcript)
-    print("---------------------------------------------")
-    qa_data.answer = model_answer.get_answer()
-    qa_data.save()
-    request.session["qa_data"]=qa_data.id
+    # qa_data.answer = model_answer.get_answer()
+    # qa_data.save()
+    request.session["question"]=uploaded_question
+    # request.session["answer"]=model_answer.get_answer()
     return HttpResponseRedirect("qa_display.html")
